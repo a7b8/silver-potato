7fda54f59873cd4a87d78329af0f82888a674aa9
a7b8 <a7b8@meta.ua>
2022-06-21 17:19:50 +0300

    Added slice.go

---
+++ les7/slice.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+  "fmt"
+  "sort"
+)
+
+func main() {
+  arr := []int{4, 1, 4, -4, 6, 3, 8, 8}
+  var result = []int{}
+
+  sort.Ints(arr)
+  result = append(result, arr[0])
+
+  for i := 0; i < len(arr); i++ {
+    if arr[i] != result[len(result)-1] {
+      result = append(result, arr[i])
+    }
+  }
+
+  fmt.Println(result)
+}
