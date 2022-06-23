b5888d5b4361b1ad1fc1aab5d73ff9115d5b4824
a7b8 <a7b8@meta.ua>
2022-06-23 13:41:48 +0300

    Replaced name  var to 'input' and added var 'result'

--- les7/slice.go
+++ les7/slice.go
@@ -1,22 +1,21 @@
 package main
 
 import (
-  "fmt"
-  "sort"
+	"fmt"
+	"sort"
 )
 
 func main() {
-  arr := []int{4, 1, 4, -4, 6, 3, 8, 8}
-  var result = []int{}
+	arr := []int{4, 1, 4, -4, 6, 3, 8, 8}
+	var result = []int{}
 
-  sort.Ints(arr)
-  result = append(result, arr[0])
+	sort.Ints(arr)
+	result = append(result, arr[0])
 
-  for i := 0; i < len(arr); i++ {
-    if arr[i] != result[len(result)-1] {
-      result = append(result, arr[i])
-    }
-  }
-
-  fmt.Println(result)
+	for i := 0; i < len(arr); i++ {
+		if arr[i] != result[len(result)-1] {
+			result = append(result, arr[i])
+		}
+	}
+	fmt.Println(result)
 }
--- les7/string.go
+++ les7/string.go
@@ -1,32 +1,35 @@
 package main
 
 import (
-    "fmt"
-    "strconv"
-    "strings"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 func main() {
-    s := "1 2 3 4 5"
-    var t = []int32{}
-    ss := strings.Fields(s)
-    for _, v := range ss {
-        a, _ := strconv.Atoi(v)
-        t = append(t, int32(a))
-    }
+	input := "1 2 3 4 5"
+	var t = []int32{}
+	var result string
 
-    if len(t) == 1 {
-        fmt.Println(t[0])
-    } else {
-        max, min := t[0], t[0]
-        for _, val := range t {
-            if val > max {
-                max = val
-            }
-            if val < min {
-                min = val
-            }
-        }
-        fmt.Println(max, min)
-    }
+	ss := strings.Fields(input)
+	for _, v := range ss {
+		a, _ := strconv.Atoi(v)
+		t = append(t, int32(a))
+	}
+
+	if len(t) == 1 {
+		result = fmt.Sprint(t[0])
+	} else {
+		max, min := t[0], t[0]
+		for _, val := range t {
+			if val > max {
+				max = val
+			}
+			if val < min {
+				min = val
+			}
+		}
+		result = fmt.Sprint(max, min)
+	}
+	fmt.Println(result)
 }
